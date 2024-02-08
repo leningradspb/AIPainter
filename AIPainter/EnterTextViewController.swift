2d88922f0e92a6beb1b1f4dd5b537385467b83f5
Ed <Siniakov>
2024-02-08 21:25:02 +0300

    gradient view created

--- AIPainter/EnterTextViewController.swift
+++ AIPainter/EnterTextViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 final class EnterTextViewController: UIViewController {
-
+    private let gradientContentView = GradientView()
+    private let messageTextView = UITextView()
+    private let sendMessageButton = UIButton()
+    private let placeholder = "Enter your prompt"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setupUI()
+    }
+    
+    private func setupUI() {
+        setupGradientView()
+        setupNavigationBar()
+    }
+    
+    private func setupGradientView() {
+        view.addSubview(gradientContentView)
+        gradientContentView.startLocation = 0
+        gradientContentView.endLocation = 0.2
+        
+        gradientContentView.startColor = .violet
+        gradientContentView.endColor = .black
+        gradientContentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
     }
 
 
---
+++ AIPainter/Extensions+CustomClasses.swift
@@ -0,0 +1,100 @@
+//
+//  Extensions+CustomClasses.swift
+//  AIPainter
+//
+//  Created by Eduard Kanevskii on 08.02.2024.
+//
+
+import UIKit
+
+final class GradientView: UIView {
+    
+    var startColor:   UIColor = .black { didSet { updateColors() }}
+    var endColor:     UIColor = .white { didSet { updateColors() }}
+    var startLocation: Double =   0.05 { didSet { updateLocations() }}
+    var endLocation:   Double =   0.95 { didSet { updateLocations() }}
+    var horizontalMode:  Bool =  false { didSet { updatePoints() }}
+    var diagonalMode:    Bool =  false { didSet { updatePoints() }}
+    
+    override class var layerClass: AnyClass { return CAGradientLayer.self }
+    
+    var gradientLayer: CAGradientLayer { return layer as! CAGradientLayer }
+    
+    func updatePoints() {
+        if horizontalMode {
+            gradientLayer.startPoint = diagonalMode ? CGPoint(x: 1, y: 0) : CGPoint(x: 0, y: 0.5)
+            gradientLayer.endPoint   = diagonalMode ? CGPoint(x: 0, y: 1) : CGPoint(x: 1, y: 0.5)
+        } else {
+            gradientLayer.startPoint = diagonalMode ? CGPoint(x: 0, y: 0) : CGPoint(x: 0.5, y: 0)
+            gradientLayer.endPoint   = diagonalMode ? CGPoint(x: 1, y: 1) : CGPoint(x: 0.5, y: 1)
+        }
+    }
+    func updateLocations() {
+        gradientLayer.locations = [startLocation as NSNumber, endLocation as NSNumber]
+    }
+    func updateColors() {
+        gradientLayer.colors    = [startColor.cgColor, endColor.cgColor]
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updatePoints()
+        updateLocations()
+        updateColors()
+    }
+}
+
+
+extension UIColor {
+    /// FF4364
+    static let scarlet = UIColor(hex: "#FF4364")
+    /// 30BA8F
+    static let grass = UIColor(hex: "#30BA8F")
+    /// #5b5b5b
+    static let commonGrey = UIColor(hex: "#5b5b5b")
+    /// 370258
+    static let violet = UIColor(hex: "#370258")
+    /// #920CE5
+    static let violetLight = UIColor(hex: "#920CE5")
+}
+
+extension String {
+    func textToImage() -> UIImage? {
+        let nsString = (self as NSString)
+        let font = UIFont.systemFont(ofSize: 1024) // you can change your font size here
+        let stringAttributes = [NSAttributedString.Key.font: font]
+        let imageSize = nsString.size(withAttributes: stringAttributes)
+
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0) //  begin image context
+        UIColor.clear.set() // clear background
+        UIRectFill(CGRect(origin: CGPoint(), size: imageSize)) // set rect size
+        nsString.draw(at: CGPoint.zero, withAttributes: stringAttributes) // draw text within rect
+        let image = UIGraphicsGetImageFromCurrentImageContext() // create image from context
+        UIGraphicsEndImageContext() //  end image context
+
+        return image ?? nil
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") { cString.removeFirst() }
+
+        if (cString.count) != 6 {
+            self.init(hex: "ffffff")
+            return
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
