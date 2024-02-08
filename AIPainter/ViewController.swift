86dfb29ea13c9034b80ee8c4664b462677cc8d06
Ed <Siniakov>
2024-02-08 20:58:22 +0300

    Initial Commit

---
+++ AIPainter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIPainter
+//
+//  Created by Eduard Kanevskii on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
